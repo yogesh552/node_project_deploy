e03bec47a8f174e7112a7bba6a067ceea11cb3d4
Yogesh <yogeshmanthanitsolution@gmail.com>
2025-02-27 17:21:54 +0530

    change public ip

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // Create a connection
 const connection = mysql.createConnection({
-    host: '98.81.238.72', // Your database host
+    host: '34.228.70.119', // Your database host
     user: 'root',             // MySQL username
     password: 'Nomistake@4579', // MySQL password (use .env for security)
     database: 'test'          // Your database name
